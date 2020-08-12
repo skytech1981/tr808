e43e65f96a7e078c00a7afade55e9db851dc8e50
sali itzhaki <sali1943@hotmail.com>
2020-08-12 16:28:21 +0300

    tuning calc

--- src/App.js
+++ src/App.js
@@ -1,35 +1,54 @@
 import React , { useState } from 'react';
-import logo from './logo.svg';
+import logo from './science-fiction.svg';
+import logo2 from './logo.svg';
 import './App.css';
 
 function App() {
 
 
  const [count,setCount] = useState(0)
-
+ 
 
 function decrementCount()
 {
-  setCount(count - 1 )
+  setCount(prevCount => prevCount - 1 )
 }
-
 function incrementCount()
 {
-  setCount(count + 1 )
+  setCount(prevCount => prevCount + 1 )
 }
+function multiplyCount()
+{
+  setCount(prevCount => prevCount * 2 )
+}
+function divideCount()
+{
+  setCount(prevCount => prevCount / 2 )
+}
+function reset()
+{
+  setCount(prevCount => prevCount * 0 )
+}
+
+
 
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-       <div>
+        <div className="bord"><img src={logo} className="App-logo" alt="logo" /> </div>
+        <img src={logo2} className="App-logo2" alt="logo" /> 
+        <h1 className="counter">counter</h1>
+        <span className="count"> {count} </span>
+       <div className="box">
         <button onClick={decrementCount}>-</button>
-        <span> {count} </span>
         <button onClick={incrementCount}>+</button>
-
-       </div>
-      </header>
+        <button onClick={multiplyCount}>*</button>
+        <button onClick={divideCount}>/</button>
+        <button className="reset" onClick={reset}>reset</button>
+        
+        </div>
+       </header>
     </div>
   );
 }
