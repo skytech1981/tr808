54b9716405149505ce6bc04f2d896dd75aa8591e
sali itzhaki <sali1943@hotmail.com>
2020-08-12 14:23:09 +0300

    new count project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import React from 'react';
+import React , { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+ const [count,setCount] = useState(0)
+
+
+function decrementCount()
+{
+  setCount(count - 1 )
+}
+
+function incrementCount()
+{
+  setCount(count + 1 )
+}
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <div>
+        <button onClick={decrementCount}>-</button>
+        <span> {count} </span>
+        <button onClick={incrementCount}>+</button>
+
+       </div>
       </header>
     </div>
   );
