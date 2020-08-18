135dd2c6a2fb942f5655886b443f16ebf0af4ee5
sali itzhaki <sali1943@hotmail.com>
2020-08-18 10:07:34 +0300

    tunning pages

--- src/App.js
+++ src/App.js
@@ -5,15 +5,18 @@ import './App.css';
 import Beatbox from './pages/beatbox';
 import Home from './pages/home';
 import About from './pages/about';
-import {Route, BrowserRouter as Router} from "react-router-dom";
+import {Route, BrowserRouter as Router, Link} from "react-router-dom";
 function App() {
 
   return (
     <Router>
-
-
-
-   
+   <nav>
+     <ul className="menulist">
+         <li><Link to="/home">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/beatbox">Beatbox</Link></li>
+     </ul>
+   </nav>   
     <div className="main">
       <Route path="/home" exact component={Home}/>
       <Route path="/about" exact component={About}/>
