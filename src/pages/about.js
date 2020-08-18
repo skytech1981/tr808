09c0a4d66cdaff89bca321b5ebb6ad855ca938cd
sali itzhaki <sali1943@hotmail.com>
2020-08-18 10:16:51 +0300

    adding some css stuff

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,6 +8,7 @@ function About() {
   return (
     <div className="aboutpage">
     <p> Hello Welcome To The About Page</p>
+    <h3>Beatboxing (also beat boxing or b-boxing) is a form of vocal percussion primarily involving the art of mimicking drum machines (typically a TR-808), using one's mouth, lips, tongue, and voice.[1] It may also involve vocal imitation of turntablism, and other musical instruments. Beatboxing today is connected with hip-hop culture, often referred to as "the fifth element" of hip-hop, although it is not limited to hip-hop music.[2][3] The term "beatboxing" is sometimes used to refer to vocal percussion in general.</h3>
     </div>
   );
 }
