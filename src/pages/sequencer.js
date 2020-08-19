ac8be11ecb48eed7e616de776080be473ac1c674
sali itzhaki <sali1943@hotmail.com>
2020-08-19 19:22:42 +0300

    tone js doesnt work

--- src/pages/sequencer.js
+++ src/pages/sequencer.js
@@ -1,19 +1,43 @@
-import React , { useState } from 'react';
+import React ,{useState }from 'react';
 import kick from '../drums/kickdrum.wav';
+import snare from '../drums/snare.wav';
 import { Tone } from 'tone/build/esm/core/Tone';
+import useSound from 'use-sound';
+
+
 
 
 function Sequencer() {
 
+
    function Sequence() {
-   const kick = new Tone.player('../drums/kickdrum.wav').toMaster();
+   const kick = new Tone.player('../drums/kickdrum.wav').toDestination();
+   const snare = new Tone.player('../drums/snare.wav').toDestination();
+   let index=0;
+
+   Tone.transport.scheduleRepeat(repeat,'8n')
+   Tone.transport.start();
+
+    function repeat(){
+      let step = index % 8;
+      let kickInputs = document.querySelector('.kick input:nth-child(${step + 1})');
+      let snareInputs = document.querySelector('.snare input:nth-child(${step + 1})');
+      if (kickInputs.checked){
+        kick.start();
+      }
+      if (snareInputs.checked){
+        snare.start();
+      }
+      index ++;
+    }
+  
    }
 
 
 
     return (
         <div className="sequence">
-          <div className="kicks">
+        <div className="kick">
         <input type="checkbox"></input>
         <input type="checkbox"></input>
         <input type="checkbox"></input>
