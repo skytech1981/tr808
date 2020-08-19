7cd2dbb9cf4b09b2f6c7468399c706268427fc5c
sali itzhaki <sali1943@hotmail.com>
2020-08-19 15:50:43 +0300

    adding more page for sequence

--- src/App.js
+++ src/App.js
@@ -6,22 +6,27 @@ import Beatbox from './pages/beatbox';
 import Home from './pages/home';
 import About from './pages/about';
 import {Route, BrowserRouter as Router, Link} from "react-router-dom";
+import Sequencer from './pages/sequencer';
+
+
 function App() {
 
   return (
     <Router>
+     
    <nav>
      <ul className="menulist">
          <li><Link to="/home">Home</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/beatbox">Beatbox</Link></li>
+        <li><Link to="/Sequencer">Sequencer</Link></li>
      </ul>
    </nav>   
     <div className="main">
       <Route path="/home" exact component={Home}/>
       <Route path="/about" exact component={About}/>
       <Route path="/beatbox" exact component={Beatbox}/>
-     
+      <Route path="/sequencer" exact component={Sequencer}/>
     </div>
     </Router>
   );
---
+++ src/pages/sequencer.js
@@ -0,0 +1,24 @@
+import React , { useState } from 'react';
+
+
+function Sequencer() {
+
+
+
+
+
+    return (
+        <div className="sequence">
+        <button type="checkbox"></button>
+        <button type="checkbox"></button>
+        <button type="checkbox"></button>
+        <button type="checkbox"></button>
+        <button type="checkbox"></button>
+        <button type="checkbox"></button>
+        <button type="checkbox"></button>
+        <button type="checkbox"></button>
+       </div>
+      );
+    }
+    
+    export default Sequencer;
