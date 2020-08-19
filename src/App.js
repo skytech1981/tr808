10cef4b4a5a65984de9150fc2068a17d7063ec2d
sali itzhaki <sali1943@hotmail.com>
2020-08-19 23:04:27 +0300

    adjusting more stuff

--- src/App.js
+++ src/App.js
@@ -12,24 +12,22 @@ import Sequencer from './pages/sequencer';
 function App() {
 
   
-
   return (
     <Router>
-     
-   <nav>
-     <ul className="menulist">
-         <li className="li"><Link to="/home" style={{paddingLeft: 13, textDecoration: 'none'}}>Home</Link></li>
-        <li className="li"><Link to="/about" style={{paddingLeft: 13, textDecoration: 'none'}}>About</Link></li>
-        <li className="li"><Link to="/beatbox" style={{paddingLeft: 13, textDecoration: 'none'}}>Beatbox</Link></li>
-        <li className="li"><Link to="/Sequencer" style={{paddingLeft: 13, textDecoration: 'none'}}>Sequencer</Link></li>
-     </ul>
-   </nav>   
-    <div className="main">
-      <Route path="/home" exact component={Home}/>
-      <Route path="/about" exact component={About}/>
-      <Route path="/beatbox" exact component={Beatbox}/>
-      <Route path="/sequencer" exact component={Sequencer}/>
-    </div>
+       <nav>
+          <ul className="menulist">
+            <li className="li"><Link to="/home" style={{color:"black",paddingLeft: 13, textDecoration: 'none'}}>Home</Link></li>
+            <li className="li"><Link to="/about" style={{color:"black",paddingLeft: 13, textDecoration: 'none'}}>About</Link></li>
+            <li className="li"><Link to="/beatbox" style={{color:"black",paddingLeft: 13, textDecoration: 'none'}}>Beatbox</Link></li>
+            <li className="li"><Link to="/Sequencer" style={{color:"black",paddingLeft: 13, textDecoration: 'none'}}>Sequencer</Link></li>
+         </ul>
+      </nav>   
+        <div className="main">
+          <Route path="/home" exact component={Home}/>
+          <Route path="/about" exact component={About}/>
+          <Route path="/beatbox" exact component={Beatbox}/>
+          <Route path="/sequencer" exact component={Sequencer}/>
+        </div>
     </Router>
   );
 }
