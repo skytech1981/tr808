ca809362bd4e250945e93e84038226b93a2eb4b0
sali itzhaki <sali1943@hotmail.com>
2020-08-20 13:34:12 +0300

    adding audio files to sequencer comp

--- src/pages/sequencer.js
+++ src/pages/sequencer.js
@@ -1,73 +1,119 @@
-import React ,{useState }from 'react';
-import kick from '../drums/kickdrum.wav';
-import snare from '../drums/snare.wav';
-import { Tone } from 'tone/build/esm/core/Tone';
-import useSound from 'use-sound';
+import React ,{Component}from 'react';
+import Kick from '../drums/kickdrum.wav';
+import Snare from '../drums/snare.wav';
+import clap from '../drums/clap.wav';
+import chihat from '../drums/chihat.wav';
+import synth from '../drums/synth.wav';
+import {Howl,Howler} from 'howler';
+import '../pages/sequencer.css';
 
 
+const audioClips = [
+  {sound:Kick,label:"Kick"},
+  {sound:chihat,label:"Hihat"},
+  {sound:Snare,label:"Snare"},
+  {sound:clap,label:"Clap"},
+ {sound:synth,label:"DrumFill"}
+]
+
+class Sequencer extends Component {
+soundPlay= (src) => {
+
+  const sound= new Howl({
+    src
+  })
+sound.play();
+}
+
+  rendButtonAndSound = () => {
+        return audioClips.map((soundObj, index) => {
+          return (
+            <div className="contain">
+            <button className="button" style={{color:"white"}} key={index} onClick={() => this.soundPlay(soundObj.sound)}>
+             {soundObj.label }
+          </button>
+          </div>
+         )
+      })
+  }
+
+render(){
+Howler.volume(1.0)
+ 
+  return <div>
+{this.rendButtonAndSound()}
 
 
-function Sequencer() {
+  </div>;
 
+  }
+}
 
-   function Sequence() {
-   const kick = new Tone.player('../drums/kickdrum.wav').toDestination();
-   const snare = new Tone.player('../drums/snare.wav').toDestination();
-   let index=0;
+export default Sequencer; 
 
-   Tone.transport.scheduleRepeat(repeat,'8n')
-   Tone.transport.start();
-
-    function repeat(){
-      let step = index % 8;
-      const kickInputs = document.querySelector('.kick input:nth-child(${step + 1})');
-      const snareInputs = document.querySelector('.snare input:nth-child(${step + 1})');
-      if (kickInputs.checked){
-        kick.start();
-      }
-      if (snareInputs.checked){
-        snare.start();
-      }
-      index ++;
-    }
+ 
   
-   }
+
+  //  function Sequence() {
+  //  const kick = new Tone.player('../drums/kickdrum.wav').toDestination();
+  //  const snare = new Tone.player('../drums/snare.wav').toDestination();
+  //  let index=0;
+
+  //  Tone.transport.scheduleRepeat(Repeat,'8n')
+  //  Tone.transport.start();
+
+  //   function Repeat(){
+  //     let step = index % 8;
+  //     const kickInputs = document.querySelector('.kick input:nth-child(${step + 1})');
+  //     const snareInputs = document.querySelector('.snare input:nth-child(${step + 1})');
+  //     if (kickInputs.checked){
+  //       kick.start();
+  //     }
+  //     if (snareInputs.checked){
+  //       snare.start();
+  //     }
+  //     index ++;
+  //   }
+  
+  //  }
 
 
 
-    return (
-        <div className="sequence">
-        <div className="kick">
-        <input type="checkbox"></input>
-        <input type="checkbox"></input>
-        <input type="checkbox"></input>
-        <input type="checkbox"></input>
-        <input type="checkbox"></input>
-        <input type="checkbox"></input>
-        <input type="checkbox"></input>
-        <input type="checkbox"></input>
-        </div>
-        <div className="snare">
-        <input type="checkbox"></input>
-        <input type="checkbox"></input>
-        <input type="checkbox"></input>
-        <input type="checkbox"></input>
-        <input type="checkbox"></input>
-        <input type="checkbox"></input>
-        <input type="checkbox"></input>
-        <input type="checkbox"></input>
-        </div>
-        
-       </div>
-       
-      );
+    // return (
       
-    }
+    //     <div className="sequence">
+    //      <button onClick={play()}>PLAY MY AUDIO</button>
+    //     <div className="kick">
+    //     <input type="checkbox"></input>
+    //     <input type="checkbox"></input>
+    //     <input type="checkbox"></input>
+    //     <input type="checkbox"></input>
+    //     <input type="checkbox"></input>
+    //     <input type="checkbox"></input>
+    //     <input type="checkbox"></input>
+    //     <input type="checkbox"></input>
+    //     </div>
+    //     <div className="snare">
+    //     <input type="checkbox"></input>
+    //     <input type="checkbox"></input>
+    //     <input type="checkbox"></input>
+    //     <input type="checkbox"></input>
+    //     <input type="checkbox"></input>
+    //     <input type="checkbox"></input>
+    //     <input type="checkbox"></input>
+    //     <input type="checkbox"></input>
+    //     </div>
+        
+    //     </div>
+        
+    //   );
+      
+    // }
     
-    export default Sequencer;
+    // export default Sequencer;
 
 
-// function Demo() {
+// function Sequencer() {
 //   const [isChecked, setIsChecked] = React.useState(
 //     false
 //   );
@@ -86,17 +132,18 @@ function Sequencer() {
 //   );
 
 //   return (
-//     <Checkbox
-//       name="demo-checkbox"
-//       checked={isChecked}
-//       size={24}
-//       label="I agree to self-isolate"
-//       onChange={() => setIsChecked(!isChecked)}
-//       onMouseDown={playActive}
-//       onMouseUp={() => {
-//         isChecked ? playOff() : playOn();
-//       }}
-//     />
+//         <Checkbox
+//             name="demo-checkbox"
+//             checked={isChecked}
+//             size={24}
+//             label="I agree to self-isolate"
+//             onChange={() => setIsChecked(!isChecked)}
+//             onMouseDown={playActive}
+//             onMouseUp={() => {
+//             isChecked ? playOff() : playOn();
+//          }}
+//         />
 //   );
 // }
-// export default Demo;
+
+// export default Sequencer;
