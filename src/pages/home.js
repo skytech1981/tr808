4d49468a90c20f0291281571c333db340b4b64cb
sali itzhaki <sali1943@hotmail.com>
2020-08-22 12:58:04 +0300

    adjusting hover effects on logos

--- src/pages/home.js
+++ src/pages/home.js
@@ -10,6 +10,7 @@ function Home() {
   return (
     <div className="homepage">
      <p className="home-title">BeatBox And Audio Documentry</p>
+     
      <img className="imghome" src="https://flypaper.soundfly.com/wp-content/uploads/2016/08/808-facebook.png"/>
      <a href="https://www.roland.com/global/promos/roland_tr-808/" target="_blank"><img className="logo" src="https://www.kindpng.com/picc/m/781-7815787_thumb-image-roland-music-logo-png-transparent-png.png"/></a>
      <a href="https://reactjs.org"><img className="rjslogo" src={ require('../pages/rjslogo.png') }/></a>
