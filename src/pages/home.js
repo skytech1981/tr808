da6ea7b8ee06ff282aa7a8a8d12bcf3023578662
sali itzhaki <sali1943@hotmail.com>
2020-08-22 03:07:51 +0300

    finish editing for today

--- src/pages/home.js
+++ src/pages/home.js
@@ -12,8 +12,8 @@ function Home() {
      <p className="home-title">BeatBox And Audio Documentry</p>
      <img className="imghome" src="https://flypaper.soundfly.com/wp-content/uploads/2016/08/808-facebook.png"/>
      <a href="https://www.roland.com/global/promos/roland_tr-808/" target="_blank"><img className="logo" src="https://www.kindpng.com/picc/m/781-7815787_thumb-image-roland-music-logo-png-transparent-png.png"/></a>
-     <img className="rjslogo" src={ require('../pages/rjslogo.png') }/>
-     <img className="hjslogo" src={ require('../pages/howlerjs.png') }/>
+     <a href="https://reactjs.org"><img className="rjslogo" src={ require('../pages/rjslogo.png') }/></a>
+     <a href="https://howlerjs.com"><img className="hjslogo" src={ require('../pages/howlerjs.png') }/></a>
     </div>
   );
 }
