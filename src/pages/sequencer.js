dbb20e8955178dc4e9fa0ed4fd92870f18fc0c18
sali itzhaki <sali1943@hotmail.com>
2020-08-23 17:55:33 +0300

    fixing all audio buttons

--- src/pages/sequencer.js
+++ src/pages/sequencer.js
@@ -3,34 +3,47 @@ import Kick from '../drums/kickdrum.wav';
 import Snare from '../drums/snare.wav';
 import clap from '../drums/clap.wav';
 import chihat from '../drums/chihat.wav';
-import drumfill from '../drums/synth.wav';
+import ride from '../drums/ride.wav';
+import shake from '../drums/shake.wav';
+import tom from '../drums/tom.wav';
+import cowba from '../drums/cowba.wav';
+import clave from '../drums/clave.wav';
+import congalo from '../drums/congalo.wav';
+import snaree from '../drums/snaree.wav';
+import softhit from '../drums/softhit.wav';
+import tropicalclap from '../drums/tropicalclap.wav';
+import Perc from '../drums/Perc.wav';
+import shot from '../drums/shot.wav';
+import hatt from '../drums/hatt.wav';
+
 import {Howl,Howler} from 'howler';
+
 import '../pages/sequencer.css';
 
 
-const audioClips =  [
+const audioClips =   [
                       {sound:Kick,label:"",id:1},
                       {sound:Snare,label:"",id:2},
                       {sound:chihat,label:"",id:3},
                       {sound:clap,label:"",id:4},
                      ]
 const audioClips2 =  [
-                      {sound:drumfill,label:"",id:5},
-                      {sound:Snare,label:"",id:6},
-                      {sound:chihat,label:"",id:7},
-                      {sound:clap,label:"",id:8},
+                      {sound:ride,label:"",id:5},
+                      {sound:shake,label:"",id:6},
+                      {sound:tom,label:"",id:7},
+                      {sound:cowba,label:"",id:8},
                      ]
 const audioClips3 =  [
-                      {sound:Kick,label:"",id:9},
-                      {sound:Snare,label:"",id:10},
-                      {sound:chihat,label:"",id:11},
-                      {sound:clap,label:"",id:12},
+                      {sound:clave,label:"",id:9},
+                      {sound:congalo,label:"",id:10},
+                      {sound:snaree,label:"",id:11},
+                      {sound:hatt,label:"",id:12},
                      ]
 const audioClips4 =  [
-                      {sound:Kick,label:"",id:9},
-                      {sound:Snare,label:"",id:10},
-                      {sound:chihat,label:"",id:11},
-                      {sound:clap,label:"",id:12},
+                      {sound:softhit,label:"",id:13},
+                      {sound:tropicalclap,label:"",id:14},
+                      {sound:Perc,label:"",id:15},
+                      {sound:shot,label:"",id:16},
                      ]
 
 
@@ -99,7 +112,7 @@ rendButtonAndSound3 = () => {
 }
 
 rendButtonAndSound4 = () => {
-  return audioClips3.map((soundObj, index) => {
+  return audioClips4.map((soundObj, index) => {
     return (
       
       <div className="contain4">
